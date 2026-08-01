4670622efdade0e403dacd72b87ebcc4b83df6c0
ShaheerAhmed <levviiiw@gmail.com>
2026-08-01 16:08:40 +0500

    UPDATED

--- 01-even-odd/script.js
+++
@@ -1,7 +0,0 @@
-let num = prompt("Enter your number")
-
-if (num % 2 === 0) {
-    alert("Even")
-} else {
-    alert("Odd")
-}
--- 03-login-checker/script.js
+++
@@ -1,8 +0,0 @@
-const username = prompt("Enter your username")
-const password = prompt("Enter your password")
-
-if (username === "admin" && password === "1234") {
-    alert("Login successful")
-} else {
-    alert("login failed")
-}
